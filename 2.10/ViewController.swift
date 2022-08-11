b86ec9acd287619dde70fed1e4dd72c7d93c1724
Greed1997 <43263569+Greed1997@users.noreply.github.com>
2022-08-11 14:16:44 +0300

    Initial Commit

---
+++ 2.10/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  2.10
+//
+//  Created by Александр on 11.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
